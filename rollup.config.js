f05e7e9f2746649057f92f20754928b10a193a58
Mario A. Santini <alexmario74@gmail.com>
2019-09-16 15:24:20 +0200

    now ready for typescript projects

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,23 @@
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
+import typescript from 'rollup-plugin-typescript2';
 
 module.exports = {
-    input: 'src/app.js',
+    input: 'src/app.ts',
     output: {
         file: 'public/main.js',
+        sourcemap: true,
         format: 'iife'
     },
     plugins: [
         serve('public'),
-        livereload('public')
+        livereload('public'),
+        typescript({
+            tsconfigOverride: {
+                compilerOptions: {
+                    sourceMap: true
+                }
+            }
+        })
     ]
 };
